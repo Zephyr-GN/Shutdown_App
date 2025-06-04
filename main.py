d023eafaca51c67591e721467b5071c78d9d4416
Zephyr-GN <adrianmoraleszevallos@gmail.com>
2025-06-03 20:46:13 -0500

    boton reinicio

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ root.geometry("400x580")
 restart_time_button=PhotoImage(file="restart time.png")
 first_button=Button(root,image=restart_time_button,borderwidth=0,cursor="hand2")
 first_button.place(x=10,y=10)
+
+#Segundo boton
